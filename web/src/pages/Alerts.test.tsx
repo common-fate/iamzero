381e2d98cb8b393a03b9901d5373efccba77b3df
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2021-05-18 17:57:38 +0800

    Add permission recommendation URLs (#12)
    * Add permission recommendation URLs
    
    Add routing to the frontend to support URLs for each captured alert
    (e.g. http://localhost:9090/alerts/b49a9bc9-ce8e-4a55-ae99-362926c6034a)
    
    Return IDs of alerts to the client when captured
    
    * Fix React Router issue in test

--- web/src/pages/Alerts.test.tsx
+++ web/src/pages/Alerts.test.tsx
@@ -1,9 +1,16 @@
 import { screen } from "@testing-library/react";
 import { render } from "../test-utils";
 import Alerts from "./Alerts";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
 
 test("Alerts page smoke test", () => {
-  render(<Alerts />);
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <Alerts />
+    </Router>
+  );
   const linkElement = screen.getByText(/Active/i);
   expect(linkElement).toBeInTheDocument();
 });
